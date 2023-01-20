808401ba6a9f3a6f8b487e75b46e8b23de89ca40
shiv10 <shivanshsharma2012@gmail.com>
2023-01-20 21:41:31 +0530

    feat: fix visibility for one single egg

--- src/components/Owned.jsx
+++ src/components/Owned.jsx
@@ -3,6 +3,45 @@ import PurchaseEgg from '../assets/purchase.png'
 import { FaQuoteRight } from 'react-icons/fa';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 
+// const data = [
+//   {
+//     id: 1,
+//     image:
+//       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+//     name: 'maria ferguson',
+//     title: 'office manager',
+//     quote:
+//       'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
+//   },
+//   {
+//     id: 2,
+//     image:
+//       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+//     name: 'john doe',
+//     title: 'regular guy',
+//     quote:
+//       'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
+//   },
+//   {
+//     id: 3,
+//     image:
+//       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg',
+//     name: 'peter smith',
+//     title: 'product designer',
+//     quote:
+//       'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
+//   },
+//   {
+//     id: 4,
+//     image:
+//       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+//     name: 'susan andersen',
+//     title: 'the boss',
+//     quote:
+//       'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ',
+//   },
+// ];
+
 const data = [
   {
     id: 1,
@@ -13,33 +52,15 @@ const data = [
     quote:
       'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
   },
-  {
-    id: 2,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
-    name: 'john doe',
-    title: 'regular guy',
-    quote:
-      'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
-  },
-  {
-    id: 3,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg',
-    name: 'peter smith',
-    title: 'product designer',
-    quote:
-      'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
-  },
-  {
-    id: 4,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
-    name: 'susan andersen',
-    title: 'the boss',
-    quote:
-      'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ',
-  },
+  // {
+  //   id: 2,
+  //   image:
+  //     'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+  //   name: 'susan andersen',
+  //   title: 'the boss',
+  //   quote:
+  //     'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ',
+  // }
 ];
 
 
@@ -78,15 +99,18 @@ function Owned() {
         </div>
         <div className="section-center">
           {people.map((person, personIndex) => {
+            console.log({personIndex, index});
             const { id, image, name, title, quote } = person;
             // more stuff
             let position = 'nextSlide';
-            if (personIndex === index) {
-              position = 'activeSlide';
-            }
+            
 
             if (personIndex === (index-1) || (index === 0 && personIndex === people.length - 1) ) {
               position = 'lastSlide';
+            }
+
+            if (personIndex === index) {
+              position = 'activeSlide';
             }
             return <article className={position} key={id}>
               <div className='person-img'>
